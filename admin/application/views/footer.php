fb02e53f0a502aa6807b7e9ab49a6b40181c1140
salmanabdurrahman <salmanabdurrahman12345@gmail.com>
2025-03-11 13:47:19 +0700

    feat: change the year in the footer

--- admin/application/views/footer.php
+++ admin/application/views/footer.php
@@ -1,5 +1,5 @@
 <footer class="bg-light text-center py-3 mt-5">
-  <div class="">copyright &copy; 2024. Amikom</div>
+  <div class="">copyright &copy; 2025. Amikom</div>
 </footer>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
